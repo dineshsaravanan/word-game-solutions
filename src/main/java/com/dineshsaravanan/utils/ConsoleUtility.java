2db47297bb651aa23fe46ad806b013af1ff04db0
dinesh <dkumaraswamy@zendesk.com>
2022-02-05 18:44:46 +0800

    Adding additional functions to ConsoleUtility
    Adding additional utility functions that would allow to get input of
    desired length and range from the console input.

--- src/main/java/com/dineshsaravanan/utils/ConsoleUtility.java
+++ src/main/java/com/dineshsaravanan/utils/ConsoleUtility.java
@@ -4,29 +4,77 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleUtility {
-  public static String readLine(String prompt) throws IOException {
+  private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+  public static String readLine(String prompt) {
+    System.out.println(prompt);
     String s = "";
-
-    try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-      System.out.println(prompt);
-      s = "";
-      try {
-        s = reader.readLine();
-      } catch (IOException e) {
-        System.out.println(e);
-      }
+    try {
+      s = reader.readLine();
+    } catch (IOException e) {
+      System.out.println(e);
     }
 
     return s;
   }
 
-  public static int getIntegerFromConsole(String prompt) throws IOException {
+  public static int getIntegerFromConsole(String prompt) {
     String line = readLine(prompt);
+
+    while(!Utility.isInteger(line)) {
+      System.out.println("Error: input is not a valid integer...");
+      line = readLine(prompt);
+    }
 
     return Integer.parseInt(line);
   }
 
-  public static String getStringFromConsole(String prompt) throws IOException {
+  public static int getIntegerFromConsole(String prompt, int min) {
+    int value = getIntegerFromConsole(prompt);
+
+    while (value < min) {
+      System.out.println("Error: input is smaller than min...");
+      value = getIntegerFromConsole(prompt);
+    }
+
+    return value;
+  }
+
+  public static int getIntegerFromConsole(String prompt, int min, int max) {
+    int value = getIntegerFromConsole(prompt, min);
+
+    while (value > max) {
+      System.out.println("Error: input is greater than max...");
+      value = getIntegerFromConsole(prompt);
+    }
+
+    return value;
+  }
+
+  public static String getStringFromConsole(String prompt) {
     return readLine(prompt);
+  }
+
+  public static String getStringFromConsole(String prompt, int minSize) {
+    String s = getStringFromConsole(prompt);
+
+    while (s.length() < minSize) {
+      System.out.println("Error: Input length is smaller than min...");
+
+      s = getStringFromConsole(prompt);
+    }
+
+    return s;
+  }
+
+  public static String getStringFromConsole(String prompt, int minSize, int maxSize) {
+    String s = getStringFromConsole(prompt, minSize);
+
+    while (s.length() > maxSize) {
+      System.out.println("Error: Input length is greater than max...");
+
+      s = getStringFromConsole(prompt);
+    }
+
+    return s;
   }
 }
